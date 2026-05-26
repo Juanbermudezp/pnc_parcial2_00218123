87b6a107f72c8f334526d0d24a0a5f58ae129797
Juanbermudezp <00218123@uca.edu.sv>
2026-05-25 18:41:06 -0600

    :memo: Proyecto Base

---
+++ Paricial_00218123/src/main/java/org/example/paricial_00218123/Paricial00218123Application.java
@@ -0,0 +1,13 @@
+package org.example.paricial_00218123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paricial00218123Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Paricial00218123Application.class, args);
+    }
+
+}
---
+++ Paricial_00218123/src/test/java/org/example/paricial_00218123/Paricial00218123ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.paricial_00218123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Paricial00218123ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
